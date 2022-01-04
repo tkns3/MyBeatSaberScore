4b2c36f0eb9c0ef69e3bae3d3548de01ac1d9a39
tkns3 <64833912+tkns3@users.noreply.github.com>
2022-01-05 00:21:40 +0900

    更新日をローカルタイムで表示する

---
+++ MyBeatSaberScore/Converters/DateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MyBeatSaberScore.Convertes
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string v = (string)value;
+            if (v.Length > 0)
+            {
+                var d = DateTime.Parse(v);
+                return d.ToLocalTime().ToString("yyyy/MM/dd HH:mm:ss (ddd)");
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
