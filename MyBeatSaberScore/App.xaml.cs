b52427e0061acb5d094dce78b14ee7a2e4ee42af
tkns3 <64833912+tkns3@users.noreply.github.com>
2022-02-28 17:36:22 +0900

    未処理例外のスタックトレースをダイアログで表示する

--- MyBeatSaberScore/App.xaml.cs
+++ MyBeatSaberScore/App.xaml.cs
@@ -13,5 +13,40 @@ namespace MyBeatSaberScore
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // UIスレッドの未処理例外で発生
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+            // UIスレッド以外の未処理例外で発生
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+            // それでも処理されない例外で発生
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var exception = e.Exception;
+            HandleException(exception);
+        }
+
+        private void OnUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            var exception = e.Exception.InnerException as Exception;
+            HandleException(exception);
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+            HandleException(exception);
+        }
+
+        private void HandleException(Exception? e)
+        {
+            MessageBox.Show($"エラーが発生しました\n{e?.ToString()}");
+            Environment.Exit(1);
+        }
     }
 }
